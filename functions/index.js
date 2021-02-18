92b1340d2477ff29da18b73530d8cdaf95c4f332
Junjae Lee <ejj612o@gmail.com>
2021-02-18 19:15:03 +0900

    단순화

--- functions/index.js
+++ functions/index.js
@@ -22,19 +22,11 @@ app.get("/", async (req, res) => {
 });
 
 app.post('/', async (req, res) => {
-     const user = req.body;
+    const user = req.body;
 
     await admin.firestore().collection('users').add(user);
 
     res.status(201).send();
 })
 
-exports.user = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// exports.user = functions.https.onRequest(app);
