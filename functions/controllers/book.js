79b79324afef033b1f8115ef2fc4d01fe7e2fcf5
Junjae Lee <ejj612o@gmail.com>
2021-02-20 17:49:59 +0900

    순서 틀린거 수정

--- functions/controllers/book.js
+++ functions/controllers/book.js
@@ -1,10 +1,10 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-const db = admin.firestore();
 const express = require("express");
 const cors = require('cors');
 
 admin.initializeApp();
+const db = admin.firestore();
 
 const bookApp = express();
 
