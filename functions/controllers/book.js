7d0d157a99a7c6027ea217c476800049a9085a41
Junjae Lee <ejj612o@gmail.com>
2021-03-04 15:26:27 +0900

    전체 조회로 수정

--- functions/controllers/book.js
+++ functions/controllers/book.js
@@ -1,8 +1,8 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
-const check = require('../controllers/middle');
+const admin = require("firebase-admin");
+const check = require("../controllers/middle");
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 admin.initializeApp();
 
@@ -11,52 +11,23 @@ const bookApp = express();
 
 bookApp.use(cors({ origin: true }));
 
+const moment = require('moment');
+require('moment-timezone');
+
 // 전체 목록 조회
 bookApp.get("/", async (req, res) => {
-  const query = req.query;
-  const snapshot = await getSnapshot(query);
+  const snapshot = await db.collection("books").get();
 
-  if (snapshot === null) {
-    res.status(400).send("올바르지 않은 쿼리");
-  } else {
-    let books = [];
-    snapshot.forEach((doc) => {
-      let id = doc.id;
-      let data = doc.data();
+  const books = [];
+  snapshot.forEach((doc) => {
+    const id = doc.id;
+    const data = doc.data();
 
-      books.push({ id, ...data });
-    });
+    books.push({ id, ...data });
+  });
 
-    res.status(200).send(JSON.stringify(books));
-  }
+  res.status(200).send(JSON.stringify(books));
 });
-
-async function getSnapshot(query) {
-  try {
-    let bookRef = db.collection("books");
-    if (Object.prototype.hasOwnProperty.call(query, "text")) {
-      const snapshot = await bookRef.get();
-      const text = query.text;
-      bookRef = await snapshot.docs.filter(doc => {
-        const {title, author, publisher} = doc.data();
-        return title.includes(text) || author.includes(text) || publisher.includes(text);
-      });
-
-      return bookRef;
-    }
-    if (Object.prototype.hasOwnProperty.call(query, "len")) {
-      if (Object.prototype.hasOwnProperty.call(query, "start")) {
-        bookRef = bookRef.orderBy('title').startAfter(query.start).limit(Number(query.len));
-      } else {
-        bookRef = bookRef.orderBy('title').limit(Number(query.len));
-      }
-    }
-    return await bookRef.get();
-  } catch (e) {
-    console.log(e)
-    return null;
-  }
-}
 
 // 특정책 조회
 bookApp.get("/:bookId", async (req, res) => {
@@ -68,48 +39,78 @@ bookApp.get("/:bookId", async (req, res) => {
   res.status(200).send(JSON.stringify({ id, ...doc.data() }));
 });
 
-// 예약 추가
-bookApp.post("/:bookId/reservations", check.requireField(["password", "name", "studentId", "time", "date", "title"]), async (req, res) => {
-  const bookRef = db.collection("books").doc(req.params.bookId);
-  const reservationRef = db.collection("reservations").doc();
-  const bcrypt = require('bcrypt');
-  const saltRounds = 10;
+bookApp.post("/test", async (req, res) => {
+  moment.tz.setDefault("Asia/Seoul");
 
-  try {
-    await db.runTransaction(async t => {
-      const doc = await t.get(bookRef);
-      const resCount = doc.data().stockCount > doc.data().reservationCount;
+  console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
 
-      if (resCount) {
-        const reservation = req.body;
-        reservation.bookId = req.params.bookId;
-        reservation.isCancle = false;
-        reservation.isSold = false;
-        reservation.password = bcrypt.hashSync(reservation.password, saltRounds);
-
-        await t.set(reservationRef, reservation);
-        await t.update(bookRef, { reservationCount: admin.firestore.FieldValue.increment(1) });
-      } else {
-        throw new Error("no stock");
-      }
-
-    });
-  } catch (e) {
-    if (e.message === "no stock") {
-      res.status(421).send("남은 재고 없음");
-    } else if (e.message === "Cannot read property 'stockCount' of undefined") {
-      res.status(421).send("문서가 존재 하지 않음");
-    } else {
-      console.log(e);
-      res.status(421).send("알수없는 에러");
-    }
-  }
-  res.status(201).send();
+  res.status(200).send();
 });
+
+// 예약 추가
+bookApp.post(
+  "/:bookId/reservations",
+  check.requireField([
+    "password",
+    "name",
+    "studentId",
+    "time",
+    "date",
+    "title",
+  ]),
+  async (req, res) => {
+    const bookRef = db.collection("books").doc(req.params.bookId);
+    const reservationRef = db.collection("reservations").doc();
+    const bcrypt = require("bcrypt");
+    const saltRounds = 10;
+
+    try {
+      await db.runTransaction(async (t) => {
+        const doc = await t.get(bookRef);
+        const resCount = doc.data().stockCount > doc.data().reservationCount;
+
+        if (resCount) {
+          const reservation = req.body;
+
+          reservation.bookId = req.params.bookId;
+          reservation.isCancle = false;
+          reservation.isSold = false;
+          reservation.password = bcrypt.hashSync(
+            reservation.password,
+            saltRounds
+          );
+
+          await t.set(reservationRef, reservation);
+          await t.update(bookRef, {
+            reservationCount: admin.firestore.FieldValue.increment(1),
+          });
+        } else {
+          throw new Error("no stock");
+        }
+      });
+    } catch (e) {
+      if (e.message === "no stock") {
+        res.status(421).send("남은 재고 없음");
+      } else if (
+        e.message === "Cannot read property 'stockCount' of undefined"
+      ) {
+        res.status(421).send("문서가 존재 하지 않음");
+      } else {
+        console.log(e);
+        res.status(421).send("알수없는 에러");
+      }
+    }
+    res.status(201).send();
+  }
+);
 
 //특정 책의 예약 조회
 bookApp.get("/:bookId/reservations", async (req, res) => {
-  const snapshot = await db.collection("reservations").where("bookId", "==", req.params.bookId).where("isCancle", "==", false).get();
+  const snapshot = await db
+    .collection("reservations")
+    .where("bookId", "==", req.params.bookId)
+    .where("isCancle", "==", false)
+    .get();
 
   let reservations = [];
   snapshot.forEach((doc) => {
@@ -126,7 +127,7 @@ bookApp.get("/:bookId/reservations", async (req, res) => {
 bookApp.delete("/:bookId/reservations/:id", async (req, res) => {
   const reservationRef = db.collection("reservations").doc(req.params.id);
   const bookRef = db.collection("books").doc(req.params.bookId);
-  const bcrypt = require('bcrypt');
+  const bcrypt = require("bcrypt");
   const query = req.query;
 
   if (Object.keys(query).length === 0) {
@@ -135,7 +136,7 @@ bookApp.delete("/:bookId/reservations/:id", async (req, res) => {
   }
 
   try {
-    await db.runTransaction(async t => {
+    await db.runTransaction(async (t) => {
       const doc = await t.get(reservationRef);
       const match = await bcrypt.compare(query.password, doc.data().password);
       const isCancle = doc.data().isCancle;
@@ -149,8 +150,10 @@ bookApp.delete("/:bookId/reservations/:id", async (req, res) => {
       }
 
       if (match) {
-        await t.update(reservationRef, { "isCancle": true });
-        await t.update(bookRef, { reservationCount: admin.firestore.FieldValue.increment(-1) });
+        await t.update(reservationRef, { isCancle: true });
+        await t.update(bookRef, {
+          reservationCount: admin.firestore.FieldValue.increment(-1),
+        });
       } else {
         throw new Error("비밀번호가 다름");
       }
@@ -164,9 +167,8 @@ bookApp.delete("/:bookId/reservations/:id", async (req, res) => {
       res.status(421).send("이미 취소된 예약");
     } else if (e.message === "이미 구매한 예약") {
       res.status(421).send("이미 구매한 예약");
-    }
-    else {
-      console.log(e)
+    } else {
+      console.log(e);
       res.status(421).send("알수없는 에러");
     }
   }
@@ -176,7 +178,10 @@ bookApp.delete("/:bookId/reservations/:id", async (req, res) => {
 
 // 특정 책의 재고 조회
 bookApp.get("/:bookId/stocks", async (req, res) => {
-  const snapshot = await db.collection("stocks").where("bookId", "==", req.params.bookId).get();
+  const snapshot = await db
+    .collection("stocks")
+    .where("bookId", "==", req.params.bookId)
+    .get();
 
   let stocks = [];
   snapshot.forEach((doc) => {
