f4be5ca2663bf5bd49775d20d353422fe39c5c2b
Junjae Lee <ejj612o@gmail.com>
2021-08-10 20:06:27 +0900

    예약 취소 단순화

--- functions/controllers/book.js
+++ functions/controllers/book.js
@@ -159,26 +159,10 @@ bookApp.delete("/:bookId/reservations/:id", async (req, res) => {
             if (match) {
                 await t.update(reservationRef, { isCancel: true });
 
-                switch (state) {
-                    case "A":
-                    case "a":
-                        await t.update(bookRef, {
-                            reservationCountA: admin.firestore.FieldValue.increment(-1),
-                        });
-                        break;
-                    case "B":
-                    case "b":
-                        await t.update(bookRef, {
-                            reservationCountB: admin.firestore.FieldValue.increment(-1),
-                        });
-                        break;
-                    case "C":
-                    case "c":
-                        await t.update(bookRef, {
-                            reservationCountC: admin.firestore.FieldValue.increment(-1),
-                        });
-                        break;
-                }
+                const json = {}
+                json[keyName.getReservationCountName(state)] = admin.firestore.FieldValue.increment(-1)
+
+                await t.update(bookRef, json);
             } else {
                 throw new Error("비밀번호가 다름");
             }
