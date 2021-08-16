c8fc1e760eafe2a091f853344fcfece1743197a2
조우성 <fhdufhdu@MACBOOKAIR.local>
2021-08-16 18:57:06 +0900

    fix funcion that removing invalid reservation

--- functions/controllers/admin_wooseong.js
+++ functions/controllers/admin_wooseong.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const check = require('./middle');
+const keyName = require('../controllers/getType');
 const express = require("express");
 const cors = require('cors');
 
@@ -33,25 +34,35 @@ made by WooSeong
 유효하지 않은 예약 제거
 ***/
 adminApp.get("/reservations/schedular", async (req, res) => {
-  const batch = db.batch();
   const nowDate = getStringDate();
-  const rsvDatas = await db.collection("reservations").get();
+  const rsvSnaps = await db.collection("reservations").get();
+
   try {
-    rsvDatas.forEach((doc) => {
-      const rsvData = doc.data()
-      const rsvRef = doc._ref
-      if (rsvData.date <= nowDate && !rsvData.isCancel) {
-        rsvData.isCancel = true
-        batch.update(rsvRef, rsvData);
-      }
+    rsvSnaps.forEach(async (rsvSnap) => {
+      //트랜잭션 실행
+      await db.runTransaction(async t => {
+        const rsvData = rsvSnap.data()
+        const rsvRef = rsvSnap._ref
+        const { isCancel, state, date } = rsvData;
+
+        const bookRef = await db.collection("books").doc(rsvData.bookId)
+
+        if (date <= nowDate && !isCancel) {
+          await t.update(rsvRef, { "isCancel": true });
+
+          const json = {}
+          json[keyName.getReservationCountName(state)] = admin.firestore.FieldValue.increment(-1);
+          console.log(json)
+          await t.update(bookRef, json);
+        }
+      });
     })
-    await batch.commit()
     res.status(200).send();
   }
   catch (e) {
     console.log(e)
-    res.status(421).send("알수없는 에러");
+    res.status(421).send("취소 실패 : 알수없는 에러");
   }
 });
 
-exports.admin_wooseong = functions.https.onRequest(adminApp); 
+exports.admin_wooseong = functions.https.onRequest(adminApp);
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     ...require('./controllers/book'),
     ...require('./controllers/admin'),
+    ...require('./controllers/admin_wooseong'),
     ...require('./controllers/user'),
     ...require('./controllers/reservation'),
 }
